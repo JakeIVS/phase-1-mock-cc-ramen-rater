453a6e92a73e0e23fe0e42443ff5ca93c7af5484
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-10 11:17:00 -0600

    complete challenge, with all advanced and extra advanced deliverables

--- src/index.js
+++ src/index.js
@@ -18,12 +18,65 @@ const ramenName = document.querySelector('.name');
 const restaurant = document.querySelector('.restaurant')
 const rating = document.querySelector('#rating-display')
 const comment = document.querySelector('#comment-display')
+const form = document.querySelector('#new-ramen')
 function showRamen(ramen){
-    debugger
     ramenImage.src = ramen.image;
     ramenName.textContent = ramen.name;
     restaurant.textContent = ramen.restaurant;
     rating.textContent = ramen.rating;
     comment.textContent = ramen.comment;
+}
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let newName = form.querySelector('#new-name').value;
+    let newRestaurant = form.querySelector('#new-restaurant').value;
+    let newImage = form.querySelector('#new-image').value;
+    let newRating = form.querySelector('#new-rating').value;
+    let newComment = form.querySelector('#new-comment').value;
+    const image = document.createElement('img');
+    image.src = newImage;
+    menu.appendChild(image);
+    ramenImage.src = newImage;
+    ramenName.textContent = newName;
+    restaurant.textContent = newRestaurant
+    rating.textContent = newRating;
+    comment.textContent = newComment;
+    fetch('http://localhost:3000/ramens',{
+        method:'POST',
+        headers:{
+            'content-type':'application/json',
+            'accept':'application/json'
+        },
+        body: JSON.stringify({
+            "name": newName,
+            "restaurant": newRestaurant,
+            "image": newImage,
+            "rating": newRating,
+            "comment": newComment
+        })
+    })
+    form.reset();
+})
+//creates and formats a delete button
+const deleteButton = document.createElement('button');
+deleteButton.textContent = 'Delete';
+deleteButton.style = 'color: white;background-color: black;position: relative;left: -70px;bottom: 24px;'
+document.querySelector('#ramen-detail').append(deleteButton)
+deleteButton.addEventListener('click', ()=>deleteItem())
+function deleteItem(){
+    fetch('http://localhost:3000/ramens')
+    .then(r=>r.json())
+    .then(data=>data.forEach(ramen=>{
+        if (ramen.name === ramenName.textContent) {
+            fetch(`http://localhost:3000/ramens/${ramen.id}`,{
+                method: 'DELETE',
+                headers:{
+                    'content-type':'application/json',
+                    'accepts': 'application/json'
+                }
+            })
+            .then(location.reload())
+        }
+    }))
 }
 initialize();
