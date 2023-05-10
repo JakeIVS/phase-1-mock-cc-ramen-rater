571a88d1709d74833a65f28baed8d304e6e11cfa
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-10 10:27:55 -0600

    complete first two deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // write your code here
+function initialize(){
+    fetch ('http://localhost:3000/ramens')
+    .then(r=>r.json())
+    .then((data)=>{
+        data.forEach(ramen=>{
+            const image = document.createElement('img');
+            image.src= ramen.image
+            menu.appendChild(image);
+            image.addEventListener('click', ()=>showRamen(ramen))
+        })
+        showRamen(data[0])
+    })
+}
+const menu = document.querySelector('#ramen-menu')
+const ramenImage = document.querySelector('.detail-image');
+const ramenName = document.querySelector('.name');
+const restaurant = document.querySelector('.restaurant')
+const rating = document.querySelector('#rating-display')
+const comment = document.querySelector('#comment-display')
+function showRamen(ramen){
+    debugger
+    ramenImage.src = ramen.image;
+    ramenName.textContent = ramen.name;
+    restaurant.textContent = ramen.restaurant;
+    rating.textContent = ramen.rating;
+    comment.textContent = ramen.comment;
+}
+initialize();
